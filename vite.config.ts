56a6f5847aecea81c25ed636bfc2b08b0fbaaa9f
MAK-52 <262978765+MAK-52@users.noreply.github.com>
2026-05-13 23:34:27 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    plugins: [react(), tailwindcss()],
+export default defineConfig({
+  plugins: [react()],
+  base: '/MAK-Smart-tools/',
+})
+
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
